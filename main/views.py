e04107b03a8fea091f03d1a7ef9a36acbbba469c
kayzaa nuur <kayza.azura@gmail.com>
2023-09-11 14:35:45 +0700

    baru

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+mhs_name = 'Pak Bepe'
+mhs_class = 'PBP A'
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': mhs_name,
+        'class': mhs_class,
     }
-
+    
     return render(request, "main.html", context)
